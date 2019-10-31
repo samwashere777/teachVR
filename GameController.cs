40e0b67b9dda47c79801d3f96057dfadba9c9eb6
SimonForinash <55114312+SimonForinash@users.noreply.github.com>
2019-10-30 20:17:29 -0700

    Add files via upload
    Whack-a-Mole

---
+++ GameController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Timer Variables
+    public TextMesh timerText;      // Reference to our Unity Text
+    public TextMesh createdBy;
+    public float gameTimer = 30f;   // 30 second game timer
+    public float restartGameTimer;  // Restarts after 4 seconds
+
+    // Mole variables
+    public GameObject moleContainer; // reference to our moleContainer in Unity
+    private Mole[] moles;
+    public float showMoleTimer = 1.5f; // show moles for 1.5 seconds
+
+    // Hammer
+    public GameObject hammer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Put all moles into our list moles
+        moles = moleContainer.GetComponentsInChildren<Mole>();
+
+        //Debug.Log("Number of Moles: " + moles.Length);  // print moles to the screen
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update the game timer
+        gameTimer -= Time.deltaTime; // subtracts a second from the game timer
+
+        // Check that the game timer is greater than 0 seconds
+        if (gameTimer > 0f)
+        {
+            // createdBy text
+            createdBy.text = "Group 13 Test";
+
+            // Update Timer
+            timerText.text = "Whack-a-Mole: " + Mathf.Floor(gameTimer);
+
+            // Show mole if == 0
+            showMoleTimer -= Time.deltaTime;
+            if (showMoleTimer < 0f)
+            {
+                // Show the next mole randomly then reset timer
+                moles[Random.Range(0, moles.Length)].ShowMole();
+                showMoleTimer = 1.5f;
+            }
+        }
+        // Game timer < 0
+        else
+        {
+            // Update text in Unity
+            timerText.text = "Game Over";
+
+            // Restart Game
+            restartGameTimer -= Time.deltaTime;
+
+            // Show restarting Time
+            createdBy.text = "Restarting in " + restartGameTimer;
+
+            // When restart game timer reaches 0, restart game
+            if (restartGameTimer < 0)
+            {
+                gameTimer = 30f;
+                restartGameTimer = 4f;
+            }
+        }
+    }
+}
---
+++ Mole.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour
+{
+    // Show and Hide Variables
+
+    public float VisibleYHeight = 1.3f;
+    public float HiddenYHeight = -2.0f;
+    private Vector3 myNewXYZPosition; // position to move Mole
+    public float speed = 4f;          // speed the moles move
+    public float hideMoleTimer = 1.25f;
+
+    private void Awake()
+    {
+        HideMole();
+
+        // set current position
+        transform.localPosition = myNewXYZPosition;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move mole to new position
+        transform.localPosition = Vector3.Lerp(
+            transform.localPosition, 
+            myNewXYZPosition,
+            Time.deltaTime * speed);
+
+        // Hide Mole if timer less than 0
+        hideMoleTimer -= Time.deltaTime;
+        if (hideMoleTimer < 0)
+        {
+            HideMole();
+        }
+    }
+
+    // hide the mole
+    public void HideMole()
+    {
+        // Set current position to hidden
+        myNewXYZPosition = new Vector3(
+               transform.localPosition.x, 
+               HiddenYHeight,
+               transform.localPosition.z
+        );
+    }
+
+    // show the mole
+    public void ShowMole()
+    {
+        // Set current position to visible
+        myNewXYZPosition = new Vector3(
+               transform.localPosition.x,
+               VisibleYHeight,
+               transform.localPosition.z
+        );
+
+        // Reset the hideMoleTimer to 1.5 seconds before hiding
+        hideMoleTimer = 1.25f;
+    }
+}
